d3d8beb678e0d560fd6742212e99ebb20b5ad40e
Duy Tran <tphduy@gmail.com>
2020-12-15 14:26:18 +0700

    Disable Linux supports

--- Tests/ActivityAnimationTests/ActivityAnimationTests.swift
+++ Tests/ActivityAnimationTests/ActivityAnimationTests.swift
@@ -1,15 +1,4 @@
 import XCTest
 @testable import ActivityAnimation
 
-final class ActivityAnimationTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ActivityAnimation().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
-}
+final class ActivityAnimationTests: XCTestCase {}
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import ActivityAnimationTests
-
-var tests = [XCTestCaseEntry]()
-tests += ActivityAnimationTests.allTests()
-XCTMain(tests)
